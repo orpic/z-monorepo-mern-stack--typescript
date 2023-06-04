15a9705d3eb28c46f3b7dbf5df43bd77c397ee83
Shobhit <121398721+orpic@users.noreply.github.com>
2023-06-04 22:45:30 +0530

    utilising from lib folder

--- apps/react-monorepo-multiple-apps/src/app/app.tsx
+++ apps/react-monorepo-multiple-apps/src/app/app.tsx
@@ -1,5 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
+import { SharedUiComponent } from '@react-monorepo-multiple-apps/shared-ui-component';
 
 // import NxWelcome from './nx-welcome';
 
@@ -8,6 +9,7 @@ export function App() {
     <div>
       <h1>Hello world!</h1>
       {/* <NxWelcome title="react-monorepo-multiple-apps" /> */}
+      <SharedUiComponent />
     </div>
   );
 }
