83e982bcea2b8c1923a3211e67a1d9c6e9b577b5
Shobhit <121398721+orpic@users.noreply.github.com>
2023-06-04 21:00:08 +0530

    new lib

--- apps/react-monorepo-multiple-apps/src/app/app.tsx
+++ apps/react-monorepo-multiple-apps/src/app/app.tsx
@@ -1,12 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 
-import NxWelcome from './nx-welcome';
+// import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="react-monorepo-multiple-apps" />
+      <h1>Hello world!</h1>
+      {/* <NxWelcome title="react-monorepo-multiple-apps" /> */}
     </div>
   );
 }
---
+++ libs/shared-components/src/lib/shared-components.js
@@ -0,0 +1,9 @@
+import styles from './shared-components.module.css';
+export function SharedComponents(props) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to SharedComponents!</h1>
+    </div>
+  );
+}
+export default SharedComponents;
