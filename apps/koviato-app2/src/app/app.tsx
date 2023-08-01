26c32f2982be137d9270dcb5eca486918e9135fe
shobhit <shobhitumrao5@gmail.com>
2023-08-01 22:46:42 +0530

    firbase deploy success
    3 different apps inside a monorepo setup depoyed individually on different link through a single project

---
+++ apps/koviato-app2/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="koviato-app2" />
+    </div>
+  );
+}
+
+export default App;
